efc1c57a6964a03ed52ccf2aa65fc3916c0ed6c7
Yuriy Yaitskiy <yuuurryyy@gmail.com>
2023-10-31 20:46:46 +0200

    First commit home

--- src/main/java/ua/yura4/controllers/PeopleController.java
+++ src/main/java/ua/yura4/controllers/PeopleController.java
@@ -3,10 +3,7 @@ package ua.yura4.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ua.yura4.dao.PersonDAO;
 import ua.yura4.models.Person;
 
@@ -37,7 +34,7 @@ public class PeopleController {
         model.addAttribute("person", new Person());
         return "people/new";
     }
-
+    @PostMapping()
     public String create(@ModelAttribute("person") Person person ){
         personDAO.save(person);
         return "redirect:/people";
